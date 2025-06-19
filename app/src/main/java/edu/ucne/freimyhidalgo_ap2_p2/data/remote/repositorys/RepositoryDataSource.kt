2275a89d06ce7c7644a87044ae75a49456e26977
fahh7 <fahh7@FREIMYHP>
2025-06-18 20:49:00 -0400

    RepositoryDataSource

---
+++ app/src/main/java/edu/ucne/freimyhidalgo_ap2_p2/data/remote/repositorys/RepositoryDataSource.kt
@@ -0,0 +1,12 @@
+package edu.ucne.freimyhidalgo_ap2_p2.data.remote.repositorys
+
+import edu.ucne.freimyhidalgo_ap2_p2.data.remote.dto.RepositoryDTO
+import javax.inject.Inject
+
+class RepositoryDataSource  @Inject constructor(
+    private val repositoryManagerApi: RepositoryManajerApi
+
+) {
+    suspend fun getRepository(username:String) = repositoryManagerApi.listRepos(username)
+
+}
