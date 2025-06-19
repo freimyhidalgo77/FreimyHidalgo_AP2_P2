521bdbdf1638bb530322994e6dd912866d9efe71
fahh7 <fahh7@FREIMYHP>
2025-06-18 20:49:35 -0400

    RepositoryManajerApi

---
+++ app/src/main/java/edu/ucne/freimyhidalgo_ap2_p2/data/remote/repositorys/RepositoryManajerApi.kt
@@ -0,0 +1,15 @@
+package edu.ucne.freimyhidalgo_ap2_p2.data.remote.repositorys
+
+import edu.ucne.freimyhidalgo_ap2_p2.data.remote.dto.RepositoryDTO
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface RepositoryManajerApi {
+
+    @GET("users/{username}/repos")
+    fun listRepos(@Path("username") username: String): List<RepositoryDTO>
+
+
+}
