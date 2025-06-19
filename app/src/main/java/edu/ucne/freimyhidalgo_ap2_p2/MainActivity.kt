3fc7666b37cff5513439b3119f161fc7522b4258
fahh7 <fahh7@FREIMYHP>
2025-06-18 20:48:46 -0400

    Main

--- app/src/main/java/edu/ucne/freimyhidalgo_ap2_p2/MainActivity.kt
+++ app/src/main/java/edu/ucne/freimyhidalgo_ap2_p2/MainActivity.kt
@@ -4,44 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.rememberCoroutineScope
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.freimyhidalgo_ap2_p2.presentation.repositoryy.RepositoryListScreen
 import edu.ucne.freimyhidalgo_ap2_p2.ui.theme.FreimyHidalgo_AP2_P2Theme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FreimyHidalgo_AP2_P2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+                val scope = rememberCoroutineScope()
+
+                RepositoryListScreen(
+                    drawerState = drawerState,
+                    scope = scope
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FreimyHidalgo_AP2_P2Theme {
-        Greeting("Android")
     }
 }
