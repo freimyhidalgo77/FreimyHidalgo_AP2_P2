2fe62a6a2513ce46f48d6a4c764d31b4804d0e40
fahh7 <fahh7@FREIMYHP>
2025-07-09 21:17:29 -0400

    MainActivity

--- app/src/main/java/edu/ucne/freimyhidalgo_ap2_p2/MainActivity.kt
+++ app/src/main/java/edu/ucne/freimyhidalgo_ap2_p2/MainActivity.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.freimyhidalgo_ap2_p2.presentation.navegation.HostNavigation
 import edu.ucne.freimyhidalgo_ap2_p2.presentation.repositoryy.RepositoryListScreen
 import edu.ucne.freimyhidalgo_ap2_p2.ui.theme.FreimyHidalgo_AP2_P2Theme
 
@@ -18,13 +25,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FreimyHidalgo_AP2_P2Theme {
-                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-                val scope = rememberCoroutineScope()
-
-                RepositoryListScreen(
-                    drawerState = drawerState,
-                    scope = scope
-                )
+                val nav = rememberNavController()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    Box(
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        HostNavigation(nav)
+                    }
+                }
             }
         }
     }
