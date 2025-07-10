c7c0e9e3537d813990108478bce4a3e74b323451
fahh7 <fahh7@FREIMYHP>
2025-07-09 21:16:44 -0400

    ColaboradorListScreen

---
+++ app/src/main/java/edu/ucne/freimyhidalgo_ap2_p2/presentation/colaboradores/ColaboradorListScreen.kt
@@ -0,0 +1,254 @@
+package edu.ucne.freimyhidalgo_ap2_p2.presentation.colaboradores
+
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import edu.ucne.freimyhidalgo_ap2_p2.data.remote.dto.ColaboradorDTO
+import edu.ucne.freimyhidalgo_ap2_p2.data.remote.dto.RepositoryDTO
+import edu.ucne.freimyhidalgo_ap2_p2.presentation.repositoryy.RepositoryUIState
+import edu.ucne.freimyhidalgo_ap2_p2.presentation.repositoryy.RepositoryViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ColaboradorListScreen(
+    drawerState: DrawerState,
+    scope: CoroutineScope,
+    goToColaborators: (String) -> Unit,
+    createRepository: () -> Unit,
+    viewModel: ColaboradorViewModel = hiltViewModel()
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val context = LocalContext.current
+    var lastretentionCount by remember { mutableStateOf(0) }
+
+    LaunchedEffect(Unit) {
+        viewModel.getColaboradores("enelramon")
+    }
+
+    LaunchedEffect(uiState.colaboradors) {
+        if (uiState.colaboradors.size > lastretentionCount) {
+            Toast.makeText(context, ": ${uiState.colaboradors.lastOrNull()?.login}", Toast.LENGTH_LONG).show()
+        }
+        lastretentionCount = uiState.colaboradors.size
+    }
+
+
+    ColaboradorListBodyScreen(
+        drawerState = drawerState,
+        scope = scope,
+        uiState = uiState,
+        reloadRepository = { viewModel.getColaboradores("enelramon") },
+        goToColaborators,
+        createRepository,
+        viewModel = viewModel
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ColaboradorListBodyScreen(
+    drawerState: DrawerState,
+    scope: CoroutineScope,
+    uiState: ColaboradorUiState,
+    reloadRepository: () -> Unit,
+    goToColaborators: (String) -> Unit,
+    createRepository: () -> Unit,
+    viewModel: ColaboradorViewModel
+
+) {
+    val searchQuery by viewModel.searchQuery.collectAsStateWithLifecycle()
+    val searchResults by viewModel.searchResults.collectAsStateWithLifecycle()
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "Lista de Colaboradores",
+                        style = TextStyle(
+                            fontSize = 22.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White
+                        )
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { scope.launch { drawerState.open() } }) {}
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.secondary
+                )
+            )
+        },
+        floatingActionButton = {
+            Box(
+                contentAlignment = Alignment.BottomEnd,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                FloatingActionButton(
+                    onClick = reloadRepository,
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .align(Alignment.BottomEnd)
+                        .offset(y = (-16).dp),
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = Color.White
+                ) {
+                    Icon(Icons.Filled.Refresh, contentDescription = "")
+                }
+            }
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(16.dp)
+        )
+             {
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    items(searchResults) { colaborador ->
+                        ColaboradorRow(colaborador)
+                    }
+                }
+            }
+        }
+    }
+
+
+@Composable
+fun ColaboradorRow(
+    colaborador:ColaboradorDTO,
+
+    ) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Card(
+        shape = MaterialTheme.shapes.medium,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp),
+        elevation = CardDefaults.cardElevation(6.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .background(MaterialTheme.colorScheme.surfaceVariant),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                modifier = Modifier.weight(5f),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                            append("Login: ")
+                        }
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.Normal)) {
+                            append(colaborador.login)
+                        }
+                    },
+                    fontSize = 18.sp,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                            append("Id: ")
+                        }
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.Normal)) {
+                            append(colaborador.id.toString())
+                        }
+                    },
+                    fontSize = 18.sp,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+
+
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                            append("URL: ")
+                        }
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.Normal)) {
+                            append(colaborador.html_url ?: "No disponible")
+                        }
+                    },
+                    fontSize = 18.sp,
+                    color = MaterialTheme.colorScheme.onSurface
+
+                )
+            }
+
+            Text(
+                text = buildAnnotatedString {
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                        append("Avatar: ")
+                    }
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Normal)) {
+                        append(colaborador.avatar_url ?: "No disponible")
+                    }
+                },
+                fontSize = 18.sp,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        }
+
+     }
+}
+
