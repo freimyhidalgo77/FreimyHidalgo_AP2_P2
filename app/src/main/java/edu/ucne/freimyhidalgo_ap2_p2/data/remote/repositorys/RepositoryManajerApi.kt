ce686b3b7628b0e85f22c50afd42fe765a57a823
fahh7 <fahh7@FREIMYHP>
2025-07-09 21:18:59 -0400

    RepositoryManagerApi

--- app/src/main/java/edu/ucne/freimyhidalgo_ap2_p2/data/remote/repositorys/RepositoryManajerApi.kt
+++ app/src/main/java/edu/ucne/freimyhidalgo_ap2_p2/data/remote/repositorys/RepositoryManajerApi.kt
@@ -1,8 +1,13 @@
 package edu.ucne.freimyhidalgo_ap2_p2.data.remote.repositorys
 
 import edu.ucne.freimyhidalgo_ap2_p2.data.remote.dto.RepositoryDTO
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.PATCH
+import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
 
@@ -10,4 +15,28 @@ interface RepositoryManajerApi {
 
     @GET("users/{username}/repos")
     suspend fun listRepos(@Path("username") username: String): List<RepositoryDTO>
+
+    @POST("users/repos")
+    suspend fun createRepo(
+        @Body request: RepositoryDTO,
+    ): RepositoryDTO
+
+
+    @PATCH("repos/{username}/{repo}")
+    suspend fun updateRepo(
+        @Path("username") username: String,
+        @Body request: RepositoryDTO,
+    ): RepositoryDTO
+
+    @DELETE("repos/{username}/{repo}")
+    suspend fun deleteRepo(
+        @Path("username") username: String,
+        @Path("repo") repo: String,
+    ): Response<Unit>
+
+
 }
+
+
+
+
