2d1c6e8d5f61358ea02a1a24ea6a13be732736f0
fahh7 <fahh7@FREIMYHP>
2025-07-09 21:14:39 -0400

    ColaboradorDTO

---
+++ app/src/main/java/edu/ucne/freimyhidalgo_ap2_p2/data/remote/dto/ColaboradorDTO.kt
@@ -0,0 +1,9 @@
+package edu.ucne.freimyhidalgo_ap2_p2.data.remote.dto
+
+data class ColaboradorDTO(
+    val login: String,
+    val id: Int,
+    val avatar_url: String,
+    val html_url: String
+
+)
