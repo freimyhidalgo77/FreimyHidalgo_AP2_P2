c5b7f65721b694576273078b5b0852c962a38c82
fahh7 <fahh7@FREIMYHP>
2025-07-09 21:17:00 -0400

    ColaboradorRepository

---
+++ app/src/main/java/edu/ucne/freimyhidalgo_ap2_p2/data/repository/ColaboradorRepository.kt
@@ -0,0 +1,37 @@
+package edu.ucne.freimyhidalgo_ap2_p2.data.repository
+
+import android.util.Log
+import edu.ucne.freimyhidalgo_ap2_p2.data.remote.colaboradores.ColaboradorDataSource
+import edu.ucne.freimyhidalgo_ap2_p2.data.remote.dto.ColaboradorDTO
+import edu.ucne.freimyhidalgo_ap2_p2.data.remote.dto.RepositoryDTO
+import edu.ucne.freimyhidalgo_ap2_p2.data.remote.repositorys.RepositoryDataSource
+import edu.ucne.freimyhidalgo_ap2_p2.data.remote.repositorys.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import javax.inject.Inject
+
+
+class ColaboradorRepository @Inject constructor(
+    private val dataSource: ColaboradorDataSource
+){
+    fun getColaborador(owner:String): Flow<Resource<List<ColaboradorDTO>>> = flow {
+        try{
+            emit(Resource.Loading())
+            val repository = dataSource.getColaborador(owner)
+            emit(Resource.Success(repository))
+        }catch (e: HttpException){
+            val errorMessage = e.response()?.errorBody()?.string() ?: e.message()
+            Log.e("ColaboradorRepository", "HttpException: $errorMessage")
+            emit(Resource.Error("Error de conexion $errorMessage"))
+        }catch (e: Exception){
+            Log.e("ColaboradorRepository", "Exception: ${e.message}")
+            emit(Resource.Error("Error: ${e.message}"))
+
+        }
+    }
+
+
+
+
+}
